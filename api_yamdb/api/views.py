53dcbda05d910dc3c212cf17424c8b498bc6e204
Alexander Shelpyakov <ashelpyakov@gmail.com>
2022-07-26 21:29:40 +0500

    TEST PEP8_5

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
-from rest_framework.viewsets import ModelViewSet
 from reviews.filters import CategoryGenreFilter
 from reviews.models import Category, Genre, Review, Title
 from reviews.serializers import (
